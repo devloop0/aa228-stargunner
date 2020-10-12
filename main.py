4478d1a6fabf2cf0d6392788c3b74c46e0f84332
Euirim Choi <euirim@gmail.com>
2020-10-11 20:30:48 -0700

    Implement project skeleton

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import gym
 
-env = gym.make('VideoPinball-v0')
+env = gym.make("VideoPinball-v0")
 print(env.action_space)
 print(env.observation_space)
 
-for episode_num in range(20):
+for _episode_num in range(20):
     observation = env.reset()
     episode_reward = 0
     for t in range(10000):
@@ -15,7 +15,7 @@ for episode_num in range(20):
         episode_reward += reward
 
         if done:
-            print('Episode finished after {} timesteps'.format(t + 1))
+            print("Episode finished after {} timesteps".format(t + 1))
             break
 
 env.close()
